153f0ec312ebd3b7e1764323eeae7c1fa71623f6
Keri Southwood-Smith <keri.southwood@gmail.com>
2022-06-11 17:19:03 -0400

    Complete Stage 3
    ConnectFour.java
    Add a HashMap to hold the buttons. The map is keyed with the column
    names and holds an ArrayList of the buttons in the column.
    
    ConnectFourController.java
    - Add a HashMap to hold the buttons which gets injected from ConnectFour.java.
    - Change the markCell method to playCell.
    - playCell now retrieves the column list it is in and checks for an empty cell
      starting from the bottom

--- four/ConnectFour.java
+++ four/ConnectFour.java
@@ -2,7 +2,10 @@ package four;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ConnectFour extends JFrame {
     private static final int WIDTH  = 420;
@@ -27,12 +30,20 @@ public class ConnectFour extends JFrame {
     private void addComponents() {
         List<String> rowNames = List.of("6", "5", "4", "3", "2", "1");
         List<String> colNames = List.of("A", "B", "C", "D", "E", "F", "G");
+        Map<String, ArrayList<JButton>> buttonGrid = new HashMap<>();
 
         for (var row : rowNames) {
             for (var col : colNames) {
-                this.add(createButton(col + row));
+                var button = createButton(col + row);
+                this.add(button);
+                if (!buttonGrid.containsKey(col)) {
+                    buttonGrid.put(col, new ArrayList<>());
+                }
+                buttonGrid.get(col).add(button);
             }
         }
+
+        controller.setButtonMap(buttonGrid);
     }
 
     private JButton createButton(final String name) {
